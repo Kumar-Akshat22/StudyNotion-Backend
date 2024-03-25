e8f76ce3049950d8e3d9760c90c4422e86036878
Kumar Akshat <kakshat247@gmail.com>
2024-03-25 21:50:42 +0530

    Implemented all the schema models required for the project

--- models/Course.js
+++ models/Course.js
@@ -40,7 +40,25 @@ const courseSchema = new mongoose.Schema({
 
     price:{
         type:Number,
-    }
+    },
+
+    thumbnail:{
+        type:String,
+    },
+
+    tag: {
+
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+    },
+
+    studentsEnrolled: [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User',
+            required:true,
+        }
+    ]
 });
 
 module.exports = mongoose.model("Course",courseSchema);
