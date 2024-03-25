0441870a4ab52d937eb032f5fcaa107238eedfca
Kumar Akshat <kakshat247@gmail.com>
2024-03-25 22:35:42 +0530

    Implemented the functionality ot send Email to the user and completed the OTP schema model

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
 
@@ -20,5 +21,33 @@ const OTPSchema = new mongoose.Schema({
         expires:5*60,
     }
 });
+
+// Function to send mail
+async function sendVerificationMail(email,otp){
+
+    try{
+
+        const mailResponse = await mailSender(email,"Verification mail from StudyNotion",otp);
+
+        console.log('Mail sent successfully:',mailResponse);
+
+
+    }
+
+    catch(error){
+
+        console.log('Error occured while sending mail:',error);
+        throw new error;
+    }
+
+
+}
+
+// Pre Save middleware that sends a verification mail with OTP before saving into the database
+OTPSchema.pre('save', async function(next){
+
+    await sendVerificationMail(this.email , this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP",OTPSchema);
